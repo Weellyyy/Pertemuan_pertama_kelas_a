96ebcce14233e3d51014527fb89ef047e8b35f43
Weellyyy <wildanprtm2304@gmail.com>
2024-02-27 18:50:06 +0700

    Add project files.

---
+++ Pertemuan Pertama Kelas A/Pertemuan Pertama Kelas A.cpp
@@ -0,0 +1,17 @@
+// Pertemuan Pertama Kelas A.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+	float nJejari, nLuas;
+	//numeric njejari, nluas
+	cout << "masukan nJejari";
+	//display "Masukan jejari"
+	cin >> nJejari;
+	//accept "njejari"
+	nLuas = 3.14 * nJejari * nJejari;
+	cout << "luasnya = " << nLuas << endl;
+}
